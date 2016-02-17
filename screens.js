4f629ae0577acbb3a16f396362e15c73bc2d1e41
Torin Snow <torinsnow@gmail.com>
2016-02-17 00:27:57 -0800

    rearranging folders and deleting legacy scripts

--- screens.js
+++ screens.js
@@ -1,23 +1,19 @@
 console.log('screens: start');
 //constructing the character window
 var charWin = function(tag){
+  console.log('scrnTag '+flowTag)
   flowTag = tag;
   //the character window itself
   new scrn('charWin',null,'display',true);
   //all the peices of the character window
   new scrn('charStat',null,'charWin',false);
-  new btn('grtBtn','accordion','charStat');
-  new scrn('grtStat','panel','charStat',false);
-  new btn('wisBtn','accordion','charStat');
-  new scrn('wisStat','panel','charStat',false);
-  new btn('mnpBtn','accordion','charStat');
-  new scrn('mnpStat','panel','charStat',false);
-  new btn('knoBtn','accordion','charStat');
-  new scrn('knoStat','panel','charStat',false);
-  new btn('judBtn','accordion','charStat');
-  new scrn('judStat','panel','charStat',false);
-  new btn('dsrBtn','accordion','charStat');
-  new scrn('dsrStat','panel','charStat',false);
+  new btn('grtBtn','overAccord','charStat');
+  new btn('wisBtn','overAccord','charStat');
+  new btn('mnpBtn','overAccord','charStat');
+  new btn('knoBtn','overAccord','charStat');
+  new btn('judBtn','overAccord','charStat');
+  new btn('dsrBtn','overAccord','charStat');
+  new scrn('statExplain','panel allTarg','charStat',false);
   console.log('>>loaded screen '+flowTag);
 };
 
