96befdab7345c53e77074cd5ee6dff1615811891
Ray Snow-Shiva <lawsno@gmail.com>
2016-02-21 23:26:18 -0800

    burn it with the fire.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
+statsView = function () {
+  var stats = [
+    ['growth', 'grt', 'growth is cool', 0],
+    ['wisdom', 'wis', 'wisdom is cool', 0],
+    ['manipulation', 'mnp', 'manipulation is cool', 0],
+    ['knowledge', 'kno', 'knowledge is cool', 0],
+    ['judgement', 'jdg', 'judgement is cool', 0],
+    ['desire', 'dsr', 'desire is cool', 0],
+    ['strength', 'str', 'strength is cool', 0],
+    ['speed', 'spd', 'speed is cool', 0],
+    ['stamina', 'sta', 'stamina is cool', 0]
+  ];
+
+  for (x=0; x < stats.length; x++) {
+    var abbr = stats[x][1];
+    var name = abbr + 'Btn';
+    var $statTemplate = $('.stat-button.none');
+    var currentStat = $statTemplate.clone().appendTo('#stat-buttons');
+
+    currentStat.removeClass('none').html(stats[x][0]);
+    currentStat.mousedown(function(){
+      console.log($(this).text());
+      $('.right-column').load('text/' + $(this).text() + 'Intro.txt');
+    })
+  }
+}
+
+ 
+
+
 $(document).ready(function(){
-  $('#display').load('screenInfo.html', function(){
-    $('#grtBtn').mousedown(function(){
-      $('#csExplain').load('text/grtIntro.txt')
-      $('#csExplain').show();
-    });
-    $('#wisBtn').mousedown(function(){
-      $('#csExplain').load('text/wisIntro.txt')
-      $('#csExplain').show();
-    });
-    $('#mnpBtn').mousedown(function(){
-      $('#csExplain').load('text/mnpIntro.txt')
-      $('#csExplain').show();
-    });
-    $('#knoBtn').mousedown(function(){
-      $('#csExplain').load('text/knoIntro.txt')
-      $('#csExplain').show();
-    });
-    $('#judBtn').mousedown(function(){
-      $('#csExplain').load('text/judIntro.txt')
-      $('#csExplain').show();
-    });
-    $('#dsrBtn').mousedown(function(){
-      $('#csExplain').load('text/dsrIntro.txt')
-      $('#csExplain').show();
-    });
-  });
+  statsView();
 });
